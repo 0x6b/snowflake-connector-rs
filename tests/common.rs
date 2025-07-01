998d684ac19650c0ec3ffda617754fe28226a560
kaoru <k@warpnine.io>
2025-07-02 00:37:17 +0900

    feat: add conditional compilation for external browser auth in tests

--- tests/common.rs
+++ tests/common.rs
@@ -1,5 +1,6 @@
 use snowflake_connector_rs::{Result, SnowflakeAuthMethod, SnowflakeClient, SnowflakeClientConfig};
 
+#[cfg(not(feature = "external-browser-authenticator"))]
 pub fn connect() -> Result<SnowflakeClient> {
     let username = std::env::var("SNOWFLAKE_USERNAME").expect("set SNOWFLAKE_USERNAME for testing");
     let password = std::env::var("SNOWFLAKE_PASSWORD").expect("set SNOWFLAKE_PASSWORD for testing");
@@ -13,6 +14,32 @@ pub fn connect() -> Result<SnowflakeClient> {
     let client = SnowflakeClient::new(
         &username,
         SnowflakeAuthMethod::Password(password),
+        SnowflakeClientConfig {
+            account,
+            warehouse,
+            database,
+            schema,
+            role,
+            timeout: None,
+        },
+    )?;
+
+    Ok(client)
+}
+
+#[cfg(feature = "external-browser-authenticator")]
+pub fn connect() -> Result<SnowflakeClient> {
+    let username = std::env::var("SNOWFLAKE_USERNAME").expect("set SNOWFLAKE_USERNAME for testing");
+    let account = std::env::var("SNOWFLAKE_ACCOUNT").expect("set SNOWFLAKE_ACCOUNT for testing");
+
+    let role = std::env::var("SNOWFLAKE_ROLE").ok();
+    let warehouse = std::env::var("SNOWFLAKE_WAREHOUSE").ok();
+    let database = std::env::var("SNOWFLAKE_DATABASE").ok();
+    let schema = std::env::var("SNOWFLAKE_SCHEMA").ok();
+
+    let client = SnowflakeClient::new(
+        &username,
+        SnowflakeAuthMethod::ExternalBrowser,
         SnowflakeClientConfig {
             account,
             warehouse,
